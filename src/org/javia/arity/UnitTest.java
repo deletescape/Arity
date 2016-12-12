adf6e2c946a3cae5218465501f1ff8bc6aa6ced2
deletescape <deletescape@outlook.com>
2016-12-12 09:00:52 +0100

    Start to convert unit tests to JUnit test classes

--- src/org/javia/arity/UnitTest.java
+++ src/org/javia/arity/UnitTest.java
@@ -288,19 +288,6 @@ class TestEval {
     }
 }
 
-
-class FormatCase {
-    public FormatCase(int rounding, double v, String s) {
-        this.rounding = rounding;
-        this.val = v;
-        this.res = s;
-    }
-    
-    public int rounding;
-    public double val;
-    public String res;            
-}
-
 class SizeCase {
     public SizeCase(int size, String v, String s) {
         this.size = size;
@@ -311,111 +298,6 @@ class SizeCase {
     public int size;
     public String val;
     public String res;
-}
-
-class TestFormat {
-    static FormatCase cases[] = { 
-        new FormatCase(0, 0.1, "0.1"),
-        new FormatCase(0, 0.12, "0.12"),
-        new FormatCase(0, 0.001, "0.001"),
-        new FormatCase(0, 0.0012, "0.0012"),
-        new FormatCase(0, 0.0000001, "1E-7"),
-        new FormatCase(0, 0.00000012, "1.2E-7"),
-        new FormatCase(0, 0.123456789012345, "0.123456789012345"),
-
-        new FormatCase(0, 0, "0"),
-        new FormatCase(0, 1, "1"),
-        new FormatCase(0, 12, "12"),
-        new FormatCase(0, 1234567890.,   "1234567890"), 
-        new FormatCase(0, 1000000000.,   "1000000000"),
-        
-        new FormatCase(0, 1.23456789012345,  "1.23456789012345"),
-        new FormatCase(0, 12345.6789012345,  "12345.6789012345"),
-        new FormatCase(0, 1234567890.12345,  "1234567890.12345"),  
-        new FormatCase(0, 123456789012345.,   "1.23456789012345E14"), 
-        new FormatCase(0, 100000000000000.,   "1E14"),
-        new FormatCase(0, 120000000000000.,   "1.2E14"),
-        new FormatCase(0, 100000000000001.,   "1.00000000000001E14"),
-
-        new FormatCase(2, 0.1, "0.1"),
-        new FormatCase(2, 0.00000012, "1.2E-7"),
-        new FormatCase(2, 0.123456789012345, "0.12345678901235"),
-
-        new FormatCase(2, 0, "0"),
-        
-        new FormatCase(2, 1.23456789012345,  "1.2345678901235"),
-        new FormatCase(3, 1.23456789012345,  "1.234567890123"),
-        
-        new FormatCase(0, 12345.6789012345,  "12345.6789012345"),
-        new FormatCase(2, 1234567890.12345,  "1234567890.1235"),  
-        new FormatCase(3, 123456789012345.,  "1.234567890123E14"), 
-        new FormatCase(2, 100000000000001.,  "1E14"),
-
-        new FormatCase(0, 12345678901234567.,   "1.2345678901234568E16"),
-        new FormatCase(2, 12345678901234567.,   "1.2345678901235E16"),
-
-        new FormatCase(0, 99999999999999999.,   "1E17"),
-        new FormatCase(0, 9999999999999999.,    "1E16"),
-        new FormatCase(0, 999999999999999.,     "9.99999999999999E14"),
-        new FormatCase(2, 999999999999999.,     "1E15"),
-        new FormatCase(2, 999999999999994.,     "9.9999999999999E14"),
-
-        new FormatCase(2, MoreMath.log2(1+.00002), "0.000028853612282487"),
-        new FormatCase(0, 4E-4, "0.0004"),
-        new FormatCase(0, 1e30, "1E30"),
-    };
-
-    static boolean testFormat() {
-        boolean ret = true;
-        for (int i = 0; i < cases.length; ++i) {
-            FormatCase c = cases[i];
-            double v = Double.parseDouble(c.res);
-            if (c.rounding == 0 && v != c.val) {
-                System.out.println("wrong test? " + c.res + " " + v + " " + c.val);
-            }
-            String res = Util.doubleToString(c.val, c.rounding);
-            if (!res.equals(c.res)) {
-                System.out.println("Expected '" + c.res + "', got '" + res + "'. " + Double.toString(c.val));
-                ret = false;
-            }
-            int nKeep = c.rounding == 0 ? 17 : 15 - c.rounding;
-            //System.out.println("" + Double.toString(c.val) + " " + Util.round(c.val, nKeep) + " " + c.res + ", got " + res);
-        }
-        return ret;
-    }
-
-    static SizeCase[] sizeCases = {
-        new SizeCase(9,  "1111111110", "1.11111E9"),
-        new SizeCase(10, "1111111110", "1111111110"),
-        new SizeCase(10,"11111111110", "1.11111E10"),
-        new SizeCase(10, "12.11111E9", "12.11111E9"),
-        new SizeCase(9,  "12.34567E9", "12.3456E9"),
-        new SizeCase(9,  "12345678E3", "1.2345E10"),
-        new SizeCase(9, "-12345678E3", "-1.234E10"),
-
-        new SizeCase(9, "-0.00000007", "-0.000000"),
-
-        new SizeCase(5, "-1.23E123", "-1.23E123"),
-        new SizeCase(5, "-1.2E123", "-1.2E123"),
-        new SizeCase(5, "-1E123", "-1E123"),
-        new SizeCase(2, "-1", "-1"),
-        new SizeCase(1, "-1", "-1"),
-        new SizeCase(1, "-0.02", "-0.02"),
-        new SizeCase(1, "0.02", "0"),
-    };
-        
-    static boolean testSizeCases() {
-        boolean ret = true;
-        for (SizeCase c : sizeCases) {
-            String truncated = Util.sizeTruncate(c.val, c.size);
-            if (!truncated.equals(c.res)) {
-                System.out.println("sizeTruncate(" + c.val + ", " + c.size + "): got '" + truncated + 
-                                   "' expected '" + c.res + "'");
-                ret = false;
-            }
-        }
-        return ret;
-    }
 }
 
 /**
@@ -571,15 +453,11 @@ public class UnitTest {
         check(Util.shortApprox(-1.235, 0.02), -1.24);
         check(Util.shortApprox(-1.235, 0.4),  -1.2000000000000002);
 
-        check(TestFormat.testFormat());
-
         check(TestEval.testEval());
 
         check(testRecursiveEval());
 
         check(testFrame());
-
-        check(TestFormat.testSizeCases());
 
         if (!allOk) {
             System.out.println("\n*** Some tests FAILED ***\n");
---
+++ src_test/org/javia/arity/FormatTest.java
@@ -0,0 +1,116 @@
+package org.javia.arity;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class FormatTest {
+
+  @Test
+  public void formatCase1() {
+    assertThat(Util.doubleToString(0.1, 0), is("0.1"));
+    assertThat(Util.doubleToString(0.12, 0), is("0.12"));
+    assertThat(Util.doubleToString(0.001, 0), is("0.001"));
+    assertThat(Util.doubleToString(0.0012, 0), is("0.0012"));
+    assertThat(Util.doubleToString(0.0000001, 0), is("1E-7"));
+    assertThat(Util.doubleToString(0.00000012, 0), is("1.2E-7"));
+    assertThat(Util.doubleToString(0.123456789012345, 0), is("0.123456789012345"));
+  }
+
+  @Test
+  public void formatCase2() {
+    assertThat(Util.doubleToString(0, 0), is("0"));
+    assertThat(Util.doubleToString(1, 0), is("1"));
+    assertThat(Util.doubleToString(12, 0), is("12"));
+    assertThat(Util.doubleToString(1234567890., 0), is("1234567890"));
+    assertThat(Util.doubleToString(1000000000., 0), is("1000000000"));
+  }
+
+  @Test
+  public void formatCase3() {
+    assertThat(Util.doubleToString(1.23456789012345, 0), is("1.23456789012345"));
+    assertThat(Util.doubleToString(12345.6789012345, 0), is("12345.6789012345"));
+    assertThat(Util.doubleToString(1234567890.12345, 0), is("1234567890.12345"));
+    assertThat(Util.doubleToString(123456789012345., 0), is("1.23456789012345E14"));
+    assertThat(Util.doubleToString(100000000000000., 0), is("1E14"));
+    assertThat(Util.doubleToString(120000000000000., 0), is("1.2E14"));
+    assertThat(Util.doubleToString(100000000000001., 0), is("1.00000000000001E14"));
+  }
+
+  @Test
+  public void formatCase4() {
+    assertThat(Util.doubleToString(0.1, 2), is("0.1"));
+    assertThat(Util.doubleToString(0.00000012, 2), is("1.2E-7"));
+    assertThat(Util.doubleToString(0.123456789012345, 2), is("0.12345678901235"));
+  }
+
+  @Test
+  public void formatCase5() {
+    assertThat(Util.doubleToString(0, 2), is("0"));
+  }
+
+  @Test
+  public void formatCase6() {
+    assertThat(Util.doubleToString(1.23456789012345, 2), is("1.2345678901235"));
+    assertThat(Util.doubleToString(1.23456789012345, 3), is("1.234567890123"));
+  }
+
+  @Test
+  public void formatCase7() {
+    assertThat(Util.doubleToString(12345.6789012345, 0), is("12345.6789012345"));
+    assertThat(Util.doubleToString(1234567890.12345, 2), is("1234567890.1235"));
+    assertThat(Util.doubleToString(123456789012345., 3), is("1.234567890123E14"));
+    assertThat(Util.doubleToString(100000000000001., 2), is("1E14"));
+  }
+
+  @Test
+  public void formatCase8() {
+    assertThat(Util.doubleToString(12345678901234567., 0), is("1.2345678901234568E16"));
+    assertThat(Util.doubleToString(12345678901234567., 2), is("1.2345678901235E16"));
+  }
+
+  @Test
+  public void formatCase9() {
+    assertThat(Util.doubleToString(99999999999999999., 0), is("1E17"));
+    assertThat(Util.doubleToString(9999999999999999., 0), is("1E16"));
+    assertThat(Util.doubleToString(999999999999999., 0), is("9.99999999999999E14"));
+    assertThat(Util.doubleToString(999999999999999., 2), is("1E15"));
+    assertThat(Util.doubleToString(999999999999994., 2), is("9.9999999999999E14"));
+  }
+
+  @Test
+  public void formatCase10() {
+    assertThat(Util.doubleToString(MoreMath.log2(1 + .00002), 2), is("0.000028853612282487"));
+    assertThat(Util.doubleToString(4E-4, 0), is("0.0004"));
+    assertThat(Util.doubleToString(1e30, 0), is("1E30"));
+  }
+
+  @Test
+  public void sizeCase1() {
+    assertThat(Util.sizeTruncate("1111111110", 9), is("1.11111E9"));
+    assertThat(Util.sizeTruncate("1111111110", 10), is("1111111110"));
+    assertThat(Util.sizeTruncate("11111111110", 10), is("1.11111E10"));
+    assertThat(Util.sizeTruncate("12.11111E9", 10), is("12.11111E9"));
+    assertThat(Util.sizeTruncate("12.34567E9", 9), is("12.3456E9"));
+    assertThat(Util.sizeTruncate("12345678E3", 9), is("1.2345E10"));
+    assertThat(Util.sizeTruncate("-12345678E3", 9), is("-1.234E10"));
+  }
+
+  @Test
+  public void sizeCase2() {
+    assertThat(Util.sizeTruncate("-0.00000007", 9), is("-0.000000"));
+  }
+
+  @Test
+  public void sizeCase3() {
+    assertThat(Util.sizeTruncate("-1.23E123", 5), is("-1.23E123"));
+    assertThat(Util.sizeTruncate("-1.2E123", 5), is("-1.2E123"));
+    assertThat(Util.sizeTruncate("-1E123", 5), is("-1E123"));
+    assertThat(Util.sizeTruncate("-1", 2), is("-1"));
+    assertThat(Util.sizeTruncate("-1", 1), is("-1"));
+    assertThat(Util.sizeTruncate("-0.02", 1), is("-0.02"));
+    assertThat(Util.sizeTruncate("0.02", 1), is("0"));
+  }
+
+}
