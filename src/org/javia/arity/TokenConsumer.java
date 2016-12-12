260146fdef09dccb1979ce2395092f74f6c95e6f
deletescape <deletescape@outlook.com>
2016-12-12 11:40:02 +0100

    refactoring

--- src/org/javia/arity/TokenConsumer.java
+++ src/org/javia/arity/TokenConsumer.java
@@ -1,12 +1,9 @@
 /*
  * Copyright (C) 2007-2008 Mihai Preda.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,8 +14,8 @@
 package org.javia.arity;
 
 abstract class TokenConsumer {
-    void start() {
-    }
+  void start() {
+  }
 
-    abstract void push(Token token) throws SyntaxException;
+  abstract void push(Token token) throws SyntaxException;
 }
