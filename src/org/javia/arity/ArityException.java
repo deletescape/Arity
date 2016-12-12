5cc307a02dc982dfe628299a6c2990689c5849ed
deletescape <deletescape@outlook.com>
2016-12-12 08:33:20 +0100

    add serial version UIDs

--- src/org/javia/arity/ArityException.java
+++ src/org/javia/arity/ArityException.java
@@ -1,12 +1,9 @@
 /*
  * Copyright (C) 2007-2009 Mihai Preda.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,17 +13,19 @@
 
 package org.javia.arity;
 
-/** 
-    Thrown when a {@link Function} is evaluated with a wrong number of arguments 
-    (when the number of arguments is not equal to the function's arity).
-*/
+/**
+ * Thrown when a {@link Function} is evaluated with a wrong number of arguments
+ * (when the number of arguments is not equal to the function's arity).
+ */
 
 public class ArityException extends RuntimeException {
-    public ArityException(String mes) {
-        super(mes);
-    }
+  private static final long serialVersionUID = -5822800773339157447L;
 
-    public ArityException(int nArgs) {
-        this("Didn't expect " + nArgs + " arguments");
-    }
+  public ArityException(String mes) {
+    super(mes);
+  }
+
+  public ArityException(int nArgs) {
+    this("Didn't expect " + nArgs + " arguments");
+  }
 }
--- src/org/javia/arity/SyntaxException.java
+++ src/org/javia/arity/SyntaxException.java
@@ -1,12 +1,9 @@
 /*
  * Copyright (C) 2007-2008 Mihai Preda.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,35 +14,36 @@
 package org.javia.arity;
 
 /**
-   Thrown when the expression can't be compiled, because it's either not
-   well-formed (e.g. "1+"), or because some simbols aren't defined (e.g. "foo+2").
-*/
+ * Thrown when the expression can't be compiled, because it's either not
+ * well-formed (e.g. "1+"), or because some simbols aren't defined (e.g. "foo+2").
+ */
 public class SyntaxException extends Exception {
-    /**
-       The expression which caused the error.
-     */
-    public String expression;
+  private static final long serialVersionUID = 4485407478273749969L;
 
-    /**
-       Explicative message (cause of error).
-     */
-    public String message;
+  /**
+   * The expression which caused the error.
+   */
+  public String expression;
 
-    /**
-       The position inside expression where the error occured.
-     */
-    public int position;
+  /**
+   * Explicative message (cause of error).
+   */
+  public String message;
 
-    public String toString() {
-        return "SyntaxException: " + message 
-            + " in '" + expression 
-            + "' at position " + position;
-    }
+  /**
+   * The position inside expression where the error occured.
+   */
+  public int position;
 
-    SyntaxException set(String str, int pos) {
-        message  = str;
-        position = pos;
-        fillInStackTrace();
-        return this;
-    }
+  @Override
+  public String toString() {
+    return "SyntaxException: " + message + " in '" + expression + "' at position " + position;
+  }
+
+  SyntaxException set(String str, int pos) {
+    message = str;
+    position = pos;
+    fillInStackTrace();
+    return this;
+  }
 }
